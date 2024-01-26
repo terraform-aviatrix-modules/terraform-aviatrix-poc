49be06cbfa3c1fdac5f05321696142265334b0b8
Dennizz <root@ipaddr.nl>
2024-01-26 17:44:21 +0100

    updates

--- locals.tf
+++ locals.tf
@@ -7,7 +7,9 @@ locals {
   spokes   = coalesce(var.datamodel.spokes, {})   #If spokes is not found in var.datamodel or is empty, return an empty map
 
   #DCF Config
-  dcf_policies = try(var.datamodel.dcf_policies, {}) #If dcf_policies is not found in var.datamodel or is empty, return an empty map
+  dcf_policies = coalesce(var.datamodel.dcf.dcf_policies, {}) #If dcf_policies is not found in var.datamodel or is empty, return an empty map
+  dcf_enable   = try(var.datamodel.dcf.dcf_enable, false)
+  smart_groups = coalesce(var.datamodel.dcf.smart_groups, {})
 
   #Network segmentation
   network_domains     = try(var.datamodel.network_segmentation.network_domains, [])     #If network_domains is not found in var.datamodel or is empty, return an empty list
--- main.tf
+++ main.tf
@@ -37,14 +37,61 @@ module "network_domains" {
   depends_on = [terraform_data.input_validation]
 }
 
-resource "aviatrix_distributed_firewalling_config" "enable_dcf" {
-  count                          = local.dcf_policies != {} ? 1 : 0 #If there are DCF policies provided, automatically enable DCF.
-  enable_distributed_firewalling = true
+resource "aviatrix_distributed_firewalling_config" "default" {
+  enable_distributed_firewalling = local.dcf_enable
+}
+
+resource "aviatrix_smart_group" "default" {
+  for_each = local.smart_groups
+
+  name = "smart-group"
+  selector {
+    match_expressions {
+      type         = "vm"
+      account_name = "devops"
+      region       = "us-west-2"
+      tags = {
+        k3 = "v3"
+      }
+    }
+
+    match_expressions {
+      cidr = "10.0.0.0/16"
+    }
+
+    match_expressions {
+      fqdn = "www.aviatrix.com"
+    }
+
+    match_expressions {
+      site = "site-test-0"
+    }
+  }
+
 }
 
 #DCF Policies
-# resource "aviatrix_distributed_firewalling_policy_list" "test" {
-#   for_each = {
-#     "polcies" = each.value
-#   }
-# }
+resource "aviatrix_distributed_firewalling_policy_list" "dcf_policies" {
+  count = local.dcf_policies != {} ? 1 : 0
+
+  dynamic "policies" {
+    for_each = local.dcf_policies
+    content {
+      name     = policies.value.name
+      action   = policies.value.action
+      protocol = policies.value.protocol
+
+      src_smart_groups = policies.value.src_smart_groups
+      dst_smart_groups = policies.value.dst_smart_groups
+      # port_ranges              = try(policies.value.port_ranges, {})
+      exclude_sg_orchestration = try(policies.value.exclude_sg_orchestration, null)
+      decrypt_policy           = try(policies.value.decrypt_policy, null)
+      flow_app_requirement     = try(policies.value.flow_app_requirement, null)
+      priority                 = try(policies.value.priority, null)
+      web_groups               = try(policies.value.webgroups, null)
+      logging                  = try(policies.value.logging, null)
+      watch                    = try(policies.value.watch, null)
+    }
+  }
+}
+
