9ef8b94f426acc3f85061883bb6e20e9695150ba
Dennizz <root@ipaddr.nl>
2024-08-21 09:56:03 +0200

    fix missing yamldecode

--- examples/main.tf
+++ examples/main.tf
@@ -2,5 +2,5 @@ module "poc" {
   source  = "terraform-aviatrix-modules/poc/aviatrix"
   version = "v1.0.2"
 
-  datamodel = file("datamodel.yaml")
+  datamodel = yamldecode(file("datamodel.yaml"))
 }
